4e03769db856ee09f7e197e185d3ad5832e6bfa7
jasminepeng8 <59179740+jasminepeng8@users.noreply.github.com>
2019-12-26 14:19:32 -0800

    Create globals.h

---
+++ Project 1/globals.h
@@ -0,0 +1,34 @@
+//
+//  globals.h
+//  zion_solution
+//
+//  Created by Jasmine Peng on 6/28/18.
+//  Copyright © 2018 Jasmine Peng. All rights reserved.
+//
+
+#ifndef globals_h
+#define globals_h
+
+///////////////////////////////////////////////////////////////////////////
+// Manifest constants
+///////////////////////////////////////////////////////////////////////////
+
+const int MAXROWS = 20;             // max number of rows in the arena
+const int MAXCOLS = 40;             // max number of columns in the arena
+const int MAXROBOTS = 130;          // max number of robots allowed
+const int MAXSHOTLEN = 5;           // max number of steps you can shoot
+const int INITIAL_ROBOT_HEALTH = 2;
+
+const int UP    = 0;
+const int DOWN  = 1;
+const int LEFT  = 2;
+const int RIGHT = 3;
+
+///////////////////////////////////////////////////////////////////////////
+//  Auxiliary function declarations
+///////////////////////////////////////////////////////////////////////////
+
+int decodeDirection(char dir);
+void clearScreen();
+
+#endif /* globals_h */
