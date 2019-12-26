e8f9dbbd1c5f8a0c2ea81a371db0ca06f2540714
jasminepeng8 <59179740+jasminepeng8@users.noreply.github.com>
2019-12-26 14:21:10 -0800

    Create Robot.h

---
+++ Project 1/Robot.h
@@ -0,0 +1,37 @@
+//
+//  Robot.h
+//  zion_solution
+//
+//  Created by Jasmine Peng on 6/28/18.
+//  Copyright © 2018 Jasmine Peng. All rights reserved.
+//
+
+#ifndef Robot_h
+#define Robot_h
+
+
+class Arena;  // This is needed to let the compiler know that Arena is a
+// type name, since it's mentioned in the Robot declaration.
+
+class Robot
+{
+public:
+    // Constructor
+    Robot(Arena* ap, int r, int c);
+    
+    // Accessors
+    int  row() const;
+    int  col() const;
+    
+    // Mutators
+    void move();
+    bool takeDamageAndLive();
+    
+private:
+    Arena* m_arena;
+    int    m_row;
+    int    m_col;
+    int    m_health;
+};
+
+#endif /* Robot_h */
